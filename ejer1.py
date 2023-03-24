272b852e8f86d4f9fee48b24dbbc9ad57c8f1560
anagarciasaiz <116553250+anagarciasaiz@users.noreply.github.com>
2023-03-24 11:00:40 +0100

    cambio

--- ejer1.py
+++ ejer1.py
@@ -1,8 +1,24 @@
-class libro:
-    def __init__(self, titulo, autor, paginas):
+class Libro:
+    def __init__(self, titulo, autor, año, paginas):
         self.titulo = titulo
         self.autor = autor
+        self.año = año
         self.paginas = paginas 
+        
 
     def describir(self):
-        
+        print(f"titulo: {self.titulo}")
+        print(f"autor: {self.autor}")
+        print(f"año: {self.año}")
+        print(f"paginas: {self.paginas}")
+
+
+#ejemplo
+libro = Libro("El Principito","Antoine de Saint-Exupéry","1943","120")
+libro.describir()
+
+
+
+
+
+    
